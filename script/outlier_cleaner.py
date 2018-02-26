8ba05c049bdb32d5dda005715b55a05b967b8cd6
xueweiyema@yahoo.com.cn <123456>
2018-02-26 17:55:32 +0800

    feature

---
+++ script/outlier_cleaner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    data = []
+    size = len(predictions)
+    for i in range(size):
+        error = (predictions[i][0] - net_worths[i][0])*(predictions[i][0] - net_worths[i][0])
+        data.append((ages[i][0],net_worths[i][0],error))
+    data.sort(key=lambda x:x[2])  
+      
+    cleaned_data = data[:81]
+
+    ### your code goes here
+
+    
+    return cleaned_data
+
